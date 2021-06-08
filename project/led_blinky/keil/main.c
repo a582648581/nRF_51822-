f2f4c33e0c27a37ccd67ffcab1c442732874ed44
vancnet.cheng <582648581@qq.com>
2021-06-08 22:41:26 +0800

    流水灯

--- project/led_blinky/keil/main.c
+++ project/led_blinky/keil/main.c
@@ -9,7 +9,23 @@ int main()
 {
 		//GPIO初始化
 		//初始化LED1
+		//nrf_gpio_cfg_output(LED_1);
+	  //点亮LED1
+		//nrf_gpio_pin_write(LED_1,0);
+	  //nrf_gpio_pin_clear(LED_1);
 		
+	
+	 //流水灯
+		bsp_board_leds_init();
+	
+	  while (true)
+    {
+				for (int i = 0; i < LEDS_NUMBER; i++)
+        {
+            bsp_board_led_invert(i);
+            nrf_delay_ms(500);
+	    	}
+  	}
 	
 //  例程代码	
 //	 /* Configure board. */
